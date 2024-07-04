a2832589f49fe6a960ab719dc57c878be5fa335b
ximi1201 <167153869+ximi1201@users.noreply.github.com>
2024-07-04 16:41:05 -0400

    added js codes to show current time zone and date

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#LA");
+  let laDateElement = losAngelesElement.querySelector(".date");
+  let laTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  laDateElement.innerHTML = losAngelesTime.format("dddd, MMMM D YYYY");
+  laTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#syd");
+  let sydDateElement = sydneyElement.querySelector(".date");
+  let sydTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTime = moment().tz("Australia/Sydney");
+
+  sydDateElement.innerHTML = sydneyTime.format("dddd, MMMM D YYYY");
+  sydTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
