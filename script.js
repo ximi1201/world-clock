c76e8346a3a12aed8f1c7802e5ff48806c40f7d1
ximi1201 <167153869+ximi1201@users.noreply.github.com>
2024-07-06 14:22:27 -0400

    update link to all cities

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function updateCity(event) {
           <div class="time">${cityTime.format("h:mm:ss")}
           <small>${cityTime.format("A")}</small></div>
     </div>
-    <a href="index.html">All cities</a>`;
+    <a href="/">All cities</a>`;
 }
 updateTime();
 setInterval(updateTime, 1000);
